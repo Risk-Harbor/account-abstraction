d762b817df5bb532406e55907c085d86c4970fc7
eerkaijun <kaijuneer@gmail.com>
2023-08-27 18:04:29 +0800

    verifying paymaster deployment

---
+++ deploy/3_deploy_VerifyingPaymaster.ts
@@ -0,0 +1,22 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { DeployFunction } from 'hardhat-deploy/types'
+import { ethers } from 'hardhat'
+
+const deployVerifyingPaymaster: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const provider = ethers.provider
+  const from = await provider.getSigner().getAddress()
+  const network = await provider.getNetwork()
+
+  const entrypoint = await hre.deployments.get('EntryPoint')
+  const ret = await hre.deployments.deploy(
+    'VerifyingPaymaster', {
+      from,
+      args: [entrypoint.address, from],
+      gasLimit: 6e6,
+      log: true,
+      deterministicDeployment: true
+    })
+  console.log('==VerifyingPaymaster addr=', ret.address)
+}
+
+export default deployVerifyingPaymaster
