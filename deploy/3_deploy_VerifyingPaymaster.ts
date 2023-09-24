8e4f591e276fad177557e1405a45e2319ebc4aba
eerkaijun <kaijuneer@gmail.com>
2023-09-24 13:09:44 +0100

    flexibility for specifying paymaster signer

--- deploy/3_deploy_VerifyingPaymaster.ts
+++ deploy/3_deploy_VerifyingPaymaster.ts
@@ -1,17 +1,22 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
 import { ethers } from 'hardhat'
+import { BytesLike } from 'ethers';
+import * as dotenv from "dotenv"
+dotenv.config();
 
 const deployVerifyingPaymaster: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const provider = ethers.provider
   const from = await provider.getSigner().getAddress()
   const network = await provider.getNetwork()
+  const paymasterPrivateKey = process.env.PAYMASTER_PRIVATE_KEY as BytesLike
+  const verifyingPaymaster = new ethers.Wallet(paymasterPrivateKey, provider)
 
   const entrypoint = await hre.deployments.get('EntryPoint')
   const ret = await hre.deployments.deploy(
     'VerifyingPaymaster', {
-      from,
-      args: [entrypoint.address, from],
+      from: paymasterPrivateKey as string,
+      args: [entrypoint.address, verifyingPaymaster.address],
       gasLimit: 6e6,
       log: true,
       deterministicDeployment: false
